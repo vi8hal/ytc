5083643c1b6e6a860659aa0e935b7e715fbeb821
IB. govt <datsmeayankhan8055@gmail.com>
2025-08-21 13:54:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex i

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-2", className)}>
-      <MessageSquareDashed className="h-6 w-6 text-primary" />
-      <span className="text-lg font-bold font-headline">DCX1</span>
+      <MessageSquareDashed className="h-7 w-7 text-primary" />
+      <span className="text-xl font-bold font-headline">DCX1</span>
     </div>
   );
 }
