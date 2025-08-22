5fee9edb3dbeba5256ed8ae41681e1877763b544
IB. govt <datsmeayankhan8055@gmail.com>
2025-08-22 18:02:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-cen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -409,7 +409,7 @@ export default function LandingPage() {
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <Logo />
             <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-              Built to revolutionize engagement.
+              Vishal Singh
             </p>
           </div>
           <p className="text-center text-sm text-muted-foreground md:text-left">
